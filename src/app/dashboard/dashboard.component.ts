037f5f4bddd62f84934c58e1c0851b1faf93586a
dhara <dhara.vyas87@gmail.com>
2022-03-22 17:41:19 +0530

    update dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -15,18 +15,16 @@ export class DashboardComponent implements OnInit {
   hasError: boolean = false;
   searchResults: any = [];
   genre : any = ['Action','Comedy','Drama','Sports'];
-
+  debounce:any;
   constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
     this.getAllShows();  
   }
- 
   
   filterDataByGenre(genre: string): Shows[] {
     return this.allShowsData.filter(itemVal => itemVal.genres.indexOf(genre) >= 0);
   }
-
 
   getAllShows() {
     this.isLoadingIndicator = true;
@@ -47,19 +45,21 @@ export class DashboardComponent implements OnInit {
   
   getShowsBySearch(searchData : any) {
     this.isLoadingIndicator = true;
-    this.apiService.searchSeries(searchData).subscribe(
-      (data: any) => {
-        this.searchResults = data.map((item: any) => item.show);
-        this.searchResults.sort((a: any, b: any) => a.rating.average > b.rating.average ? -1 : 1);
-        this.hasError = false;
-      },
-      (error) => {
-        this.hasError = true;
-      },
-      () => {
-        this.isLoadingIndicator = false;
-      }
-    );
+    clearTimeout(this.debounce);
+    this.debounce = setTimeout(()=>{
+      this.apiService.searchSeries(searchData).subscribe(
+        (data: any) => {
+          this.searchResults = data.map((item: any) => item.show);
+          this.searchResults.sort((a: any, b: any) => a.rating.average > b.rating.average ? -1 : 1);
+          this.hasError = false;
+        },
+        (error) => {
+          this.hasError = true;
+        },
+        () => {
+          this.isLoadingIndicator = false;
+        }
+      );
+    },500)    
   }
- 
 }
